61afc176399fe1f6fe84de0c4b22637b5501830f
Henrique Melanda <hjdesigner23@gmail.com>
2018-05-09 13:07:19 -0300

    01 and 02 hello redux

---
+++ 01-hello-redux/main.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const counter = (state, action) => {
+	switch (action.type) {
+		case 'INCREMENT': return state + 1
+		case 'DECREMENT': return state - 1
+	}
+	if (typeof state === 'undefined') {
+		return 0
+	}
+	return state
+}
+
+console.assert(
+	counter(0, { type: 'INCREMENT' }) === 1
+)
+console.assert(
+	counter(1, { type: 'INCREMENT' }) === 2
+)
+console.assert(
+	counter(2, { type: 'DECREMENT' }) === 1
+)
+console.assert(
+	counter(5, { type: 'DECREMENT' }) === 4
+)
+console.assert(
+	counter(3, { type: 'SOMETHING' }) === 3
+)
+console.assert(
+	counter(undefined, {}) === 0
+)
