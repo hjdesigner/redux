4b5615df841dcb6f58db795ed65d1e65878ce6bd
Henrique Melanda <hjdesigner23@gmail.com>
2018-05-16 14:23:30 -0300

    Classroom 08 on redux

---
+++ 08-react-counter/src/index.js
@@ -0,0 +1,41 @@
+'use strict'
+
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import App from './app'
+
+const counter = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT': return state + 1
+    case 'DECREMENT': return state - 1
+  }
+  if (typeof state === 'undefined') {
+    return 0
+  }
+  return state
+}
+
+const store = createStore(counter)
+
+const renderApp = (NextApp) => {
+  render(
+    <AppContainer>
+      <Provider store={store}>
+        <NextApp />
+      </Provider>
+    </AppContainer>,
+    document.querySelector('[data-js="app"]')
+  )
+}
+
+renderApp(App)
+
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    const NextApp = require('./app').default
+    renderApp(NextApp)
+  })
+}
