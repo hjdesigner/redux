57ef2e913e78cb385372c3c54618cb5e6be38db6
Henrique Melanda <henrique.melanda@fcamara.com.br>
2018-07-13 16:41:59 -0300

    Class 43 Todo list and Get cep Redux

---
+++ 43-todo-list-get-cep/src/index.js
@@ -0,0 +1,49 @@
+'use strict'
+
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import App from './app'
+import rootReducer from 'reducers'
+
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+  console.log('LOGGER::will dispacth', action)
+  const nextAction = next(action)
+  console.log('LOGGER::next action', nextAction)
+  return nextAction
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger))
+
+const renderState = () => {
+  console.log('state', store.getState())
+}
+
+store.subscribe(renderState)
+renderState()
+
+const renderApp = (NextApp) => {
+  render(
+    <AppContainer>
+      <Provider store={store}>
+        <NextApp />
+      </Provider>
+    </AppContainer>,
+    document.querySelector('[data-js="app"]')
+  )
+}
+
+renderApp(App)
+
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    const NextApp = require('./app').default
+    renderApp(NextApp)
+  })
+  module.hot.accept('reducers', () => {
+    const nextReducer = require('reducers').default
+    store.replaceReducer(nextReducer)
+  })
+}
