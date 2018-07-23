9110a05a1ee0a5ba1da8c18a86b480bcdef708ec
Henrique Melanda <henrique.melanda@fcamara.com.br>
2018-07-23 15:54:24 -0300

    ReactFlix Redux class 06

---
+++ 49-reactflix/06/src/app.js
@@ -0,0 +1,50 @@
+'use strict'
+
+import React from 'react'
+import styled, { injectGlobal } from 'styled-components'
+import VideosList from 'components/videos-list'
+import VideoSingle from 'components/video-single'
+import 'normalize.css'
+import 'milligram'
+
+const App = () => (
+  <Container>
+    <Header>
+      <h1>ReactFlix</h1>
+    </Header>
+
+    <Main>
+      <VideoSingle />
+      <VideosList />
+    </Main>
+
+    <Footer>
+      &copy; 2018
+    </Footer>
+  </Container>
+)
+
+injectGlobal`
+  html, body, div[data-js="app"] {
+    height: 100%;
+  }
+`
+const headerHeight = '60px'
+const footerHeight = '30px'
+
+const Header = styled.header`
+  height: ${headerHeight};
+  background: #333;
+`
+const Container = styled.div`
+  height: 100%;
+`
+const Main = styled.main`
+  height: auto;
+`
+const Footer = styled.footer`
+  height: ${footerHeight};
+  background: #333;
+`
+
+export default App
